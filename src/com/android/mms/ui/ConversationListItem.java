8e096f990e9fc8c0532f362fb34a786c37631bb3
martincz <martincz@mokeedev.com>
2014-12-04 22:19:54 +0800

    Mms: mokee location support

--- src/com/android/mms/ui/ConversationListItem.java
+++ src/com/android/mms/ui/ConversationListItem.java
@@ -42,6 +42,9 @@ import com.android.mms.data.ContactList;
 import com.android.mms.data.Conversation;
 import com.android.mms.util.SmileyParser;
 
+import org.mokee.location.PhoneLocation;
+import org.mokee.util.MoKeeUtils;
+
 /**
  * This class manages the view for given conversation.
  */
@@ -53,6 +56,7 @@ public class ConversationListItem extends RelativeLayout implements Contact.Upda
     private TextView mSubjectView;
     private TextView mFromView;
     private TextView mDateView;
+    private TextView mLocationView;
     private View mAttachmentView;
     private View mErrorIndicator;
     private QuickContactBadge mAvatarView;
@@ -86,6 +90,7 @@ public class ConversationListItem extends RelativeLayout implements Contact.Upda
         mSubjectView = (TextView) findViewById(R.id.subject);
 
         mDateView = (TextView) findViewById(R.id.date);
+        mLocationView = (TextView) findViewById(R.id.location);
         mAttachmentView = findViewById(R.id.attachment);
         mErrorIndicator = findViewById(R.id.error);
         mAvatarView = (QuickContactBadge) findViewById(R.id.avatar);
@@ -217,6 +222,11 @@ public class ConversationListItem extends RelativeLayout implements Contact.Upda
 
         // Register for updates in changes of any of the contacts in this conversation.
         ContactList contacts = conversation.getRecipients();
+
+        // Location
+        if (MoKeeUtils.isChineseLanguage()) {
+        	mLocationView.setText(PhoneLocation.getCityFromPhone(contacts.get(0).getNumber()));
+        }
 
         if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
             Log.v(TAG, "bind: contacts.addListeners " + this);
