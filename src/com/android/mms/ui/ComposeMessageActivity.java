e098c1adb3c321d2566ab8999d7818e2573b079f
martincz <martincz@mokeedev.com>
2015-01-23 04:13:24 +0800

    Mms: Direct Call (4/4)
    Change-Id: Ida612f112a054df8ed78c9745386c425753c5d66

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -52,6 +52,7 @@ import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.app.Service;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ClipData;
@@ -77,6 +78,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.VectorDrawable;
 import android.media.RingtoneManager;
+import android.mokee.hardware.ProximitySensorManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -198,7 +200,8 @@ import com.google.android.mms.pdu.SendReq;
  */
 public class ComposeMessageActivity extends Activity
         implements View.OnClickListener, TextView.OnEditorActionListener,
-        MessageStatusListener, Contact.UpdateListener, IZoomListener {
+        MessageStatusListener, Contact.UpdateListener, IZoomListener,
+        ProximitySensorManager.ProximitySensorListener {
     public static final int REQUEST_CODE_ATTACH_IMAGE     = 100;
     public static final int REQUEST_CODE_TAKE_PICTURE     = 101;
     public static final int REQUEST_CODE_ATTACH_VIDEO     = 102;
@@ -334,6 +337,8 @@ public class ComposeMessageActivity extends Activity
     private BackgroundQueryHandler mBackgroundQueryHandler;
 
     private Conversation mConversation;     // Conversation we are working in
+
+    private ProximitySensorManager mProximitySensorManager;
 
     // When mSendDiscreetMode is true, this activity only allows a user to type in and send
     // a single sms, send the message, and then exits. The message history and menus are hidden.
@@ -2002,6 +2007,9 @@ public class ComposeMessageActivity extends Activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mProximitySensorManager = new ProximitySensorManager(ComposeMessageActivity.this, this);
+
         mIsSmsEnabled = MmsConfig.isSmsEnabled(this);
         super.onCreate(savedInstanceState);
 
@@ -2500,6 +2508,12 @@ public class ComposeMessageActivity extends Activity
             }
         }, 100);
 
+        TelephonyManager tm = (TelephonyManager) getSystemService(Service.TELEPHONY_SERVICE);
+        if (Settings.System.getInt(mContentResolver, Settings.System.DIRECT_CALL_FOR_MMS, 0) == 1
+              && tm.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+            mProximitySensorManager.enable();
+        }
+
         mIsRunning = true;
         updateThreadIdIfRunning();
         mConversation.markAsRead(true);
@@ -2524,6 +2538,9 @@ public class ComposeMessageActivity extends Activity
         if (isRecipientsEditorVisible()) {
             mRecipientsEditor.removeTextChangedListener(mRecipientsWatcher);
         }
+
+        // always disable just to make sure we never keep it alive
+        mProximitySensorManager.disable();
 
         // remove any callback to display a progress spinner
         if (mAsyncDialog != null) {
@@ -6021,4 +6038,17 @@ public class ComposeMessageActivity extends Activity
         }
     };
 
+    @Override
+    public void onPickup() {
+        if (!getRecipients().isEmpty()) {
+            mProximitySensorManager.disable();
+
+            // get number and attach it to an Intent.ACTION_CALL, then start the Intent
+            String number = getRecipients().get(0).getNumber();
+            Intent dialIntent = new Intent(Intent.ACTION_CALL);
+            dialIntent.setData(Uri.fromParts("tel", number, null));
+            dialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(dialIntent);
+        }
+    }
 }
